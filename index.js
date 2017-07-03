07b4d4281bb9293f54f0f36be221ae77670bbeaf
Mark Greenwood <mrkjgreenwood@gmail.com>
2017-07-03 14:46:09 -0700

    Initial commit; still entering starter code

---
+++ index.js
@@ -0,0 +1,39 @@
+fs = require('fs');
+
+// songs
+imagine = ['c', 'cmaj7', 'f', 'am', 'dm', 'g', 'e7'];
+somewhere_over_the_rainbow = ['c', 'em', 'f', 'g', 'am'];
+tooManyCooks = ['c', 'g', 'f'];
+iWillFollowYouIntoTheDark = ['f', 'dm', 'bb', 'c', 'a', 'bbm'];
+babyOneMoreTime = ['cm', 'g', 'bb', 'eb', 'fm', 'ab'];
+creep = ['g', 'gsus4', 'b', 'bsus4', 'c', 'cmsus4', 'cm6'];
+army = ['ab', 'ebm7', 'dbadd9', 'fm7', 'bbm', 'abmaj7', 'ebm'];
+paperBag = ['bm7', 'e', 'c', 'g', 'b7', 'f', 'em', 'a', 'cmaj7', 'em7', 'a7', 'f7', 'b'];
+toxic = ['cm', 'eb', 'g', 'cdim', 'eb7', 'd7', 'db7', 'ab', 'gmaj7'];
+bulletproof = ['d#m', 'g#', 'b', 'f#', 'g#m', 'c#'];
+song_11 = [];
+
+var songs = [];
+var labels = [];
+var allChords = [];
+var labelCounts = [];
+var labelProbabilities = [];
+var chordCountsInLabels = {};
+var probabilityOfChordsInLabels = {};
+
+function train(chords, labels) {
+  songs.push([label, chords]);
+  labels.push(label);
+  for (var i = 0; i < chords.length; i++) {
+    if (!allChords.includes(chords[i])) {
+      allChords.push(chords[i]);
+    }
+  }
+
+  if (!!(Object.keys(labelCounts).includes(label))) {
+    labelCounts[label] = labelCounts[label] + 1;
+  }
+  else {
+    labelCounts[label] = 1;
+  }
+}
