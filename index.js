5d25c618af92cd59e1dfc6ccd92299c3fbb66c18
Mark Greenwood <mrkjgreenwood@gmail.com>
2017-07-03 17:20:48 -0700

    Ready to work on refactoring the naive-bayes app in Refactoring JavaScript

--- index.js
+++ index.js
@@ -21,7 +21,7 @@ var labelProbabilities = [];
 var chordCountsInLabels = {};
 var probabilityOfChordsInLabels = {};
 
-function train(chords, labels) {
+function train(chords, label) {
   songs.push([label, chords]);
   labels.push(label);
   for (var i = 0; i < chords.length; i++) {
@@ -37,3 +37,76 @@ function train(chords, labels) {
     labelCounts[label] = 1;
   }
 }
+
+function getNumberOfSongs() {
+  return songs.length;
+}
+
+function setLabelProbabilities() {
+  Object.keys(labelCounts).forEach(function(label) {
+    var numberOfSongs = getNumberOfSongs();
+    labelProbabilities[label] = labelCounts[label] / numberOfSongs;
+  });
+}
+
+function setChordCountsInLabels() {
+  songs.forEach(function(i) {
+    if (chordCountsInLabels[i[0]] === undefined) {
+      chordCountsInLabels[i[0]] = {};
+    }
+    i[1].forEach(function(j) {
+      if (chordCountsInLabels[i[0]][j] > 0) {
+        chordCountsInLabels[i[0]][j] = chordCountsInLabels[i[0]][j] + 1;
+      }
+      else {
+        chordCountsInLabels[i[0]][j] = 1;
+      }
+    });
+  });
+}
+
+function setProbabilityOfChordsInLabels() {
+  probabilityOfChordsInLabels = chordCountsInLabels;
+  Object.keys(probabilityOfChordsInLabels).forEach(function(i) {
+    Object.keys(probabilityOfChordsInLabels[i]).forEach(function(j) {
+      probabilityOfChordsInLabels[i][j] = probabilityOfChordsInLabels[i][j] * 1.0 / songs.length;
+    });
+  });
+}
+
+train(imagine, 'easy');
+train(somewhere_over_the_rainbow, 'easy');
+train(tooManyCooks, 'easy');
+train(iWillFollowYouIntoTheDark, 'medium');
+train(babyOneMoreTime, 'medium');
+train(creep, 'medium');
+train(paperBag, 'hard');
+train(toxic, 'hard');
+train(bulletproof, 'hard');
+
+setLabelProbabilities();
+setChordCountsInLabels();
+setProbabilityOfChordsInLabels();
+
+function classify(chords) {
+  var ttal = labelProbabilities;
+  console.log(ttal);
+  var classified = {};
+  Object.keys(ttal).forEach(function(obj) {
+    var first = labelProbabilities[obj] + 1.01;
+    chords.forEach(function(chord) {
+      var probabilityOfChordInLabel = probabilityOfChordsInLabels[obj][chord];
+      if(probabilityOfChordInLabel === undefined) {
+        first + 1.01;
+      }
+      else {
+        first = first * (probabilityOfChordInLabel + 1.01);
+      }
+    });
+    classified[obj] = first;
+  });
+  console.log(classified);
+}
+
+classify(['d', 'g', 'e', 'dm']);
+classify(['f#m7', 'a', 'dadd9', 'dmaj7', 'bm', 'bm7', 'd', 'f#m']);
