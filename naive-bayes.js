9a4d95b789b34e419eea68e982970049313d65a8
Mark Greenwood <mark.greenwood@cascadeenergy.com>
2017-07-12 09:09:47 -0700

    Added first test infrastructure

--- naive-bayes.js
+++ naive-bayes.js
@@ -109,3 +109,11 @@ function classify(chords) {
 
 classify(['d', 'g', 'e', 'dm']);
 classify(['f#m7', 'a', 'dadd9', 'dmaj7', 'bm', 'bm7', 'd', 'f#m']);
+
+var wish = require('wish');
+
+describe('the file', () => {
+  it('works', () => {
+    wish(true);
+  });
+});
