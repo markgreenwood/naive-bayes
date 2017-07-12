aedc2b955f6541366d5bf894b39de68701bd6f2e
Mark Greenwood <mark.greenwood@cascadeenergy.com>
2017-07-12 08:32:50 -0700

    Changed allChords to Set and labelCounts and labelProbabilites to Objects to enable easier handling

--- naive-bayes.js
+++ naive-bayes.js
@@ -27,9 +27,9 @@ bulletproof = ['d#m', 'g#', 'b', 'f#', 'g#m', 'c#'];
 
 var songs = [];
 var labels = [];
-var allChords = [];
-var labelCounts = [];
-var labelProbabilities = [];
+var allChords = new Set();
+var labelCounts = {};
+var labelProbabilities = {};
 var chordCountsInLabels = {};
 var probabilityOfChordsInLabels = {};
 
@@ -37,11 +37,7 @@ function train(chords, label) {
   songs.push([label, chords]);
   labels.push(label);
 
-  chords.forEach((chord) => {
-    if(!allChords.includes(chord)) {
-      allChords.push(chord);
-    }
-  });
+  chords.forEach((chord) => allChords.add(chord));
 
   if (Object.keys(labelCounts).includes(label)) {
     labelCounts[label] = labelCounts[label] + 1;
