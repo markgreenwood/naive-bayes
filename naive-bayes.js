460ffb3369dc2421447e51363cb2a67228c09f7f
Mark Greenwood <mark.greenwood@cascadeenergy.com>
2017-07-12 08:26:40 -0700

    Added fileName function for display, refactored chord loop

--- naive-bayes.js
+++ naive-bayes.js
@@ -1,3 +1,10 @@
+function fileName() {
+  var theError = new Error('here I am');
+  return theError.stack.split('\n')[1].split('/').pop().split(':')[0];
+}
+
+console.log(`Welcome to ${fileName()}!`);
+
 var easy = 'easy';
 var medium = 'medium';
 var hard = 'hard';
@@ -9,8 +16,13 @@ tooManyCooks = ['c', 'g', 'f'];
 iWillFollowYouIntoTheDark = ['f', 'dm', 'bb', 'c', 'a', 'bbm'];
 babyOneMoreTime = ['cm', 'g', 'bb', 'eb', 'fm', 'ab'];
 creep = ['g', 'gsus4', 'b', 'bsus4', 'c', 'cmsus4', 'cm6'];
-paperBag = ['bm7', 'e', 'c', 'g', 'b7', 'f', 'em', 'a', 'cmaj7', 'em7', 'a7', 'f7', 'b'];
-toxic = ['cm', 'eb', 'g', 'cdim', 'eb7', 'd7', 'db7', 'ab', 'gmaj7'];
+paperBag = ['bm7', 'e', 'c', 'g',
+  'b7', 'f', 'em', 'a',
+  'cmaj7', 'em7', 'a7', 'f7',
+  'b'];
+toxic = ['cm', 'eb', 'g', 'cdim',
+  'eb7', 'd7', 'db7', 'ab',
+  'gmaj7'];
 bulletproof = ['d#m', 'g#', 'b', 'f#', 'g#m', 'c#'];
 
 var songs = [];
@@ -22,14 +34,14 @@ var chordCountsInLabels = {};
 var probabilityOfChordsInLabels = {};
 
 function train(chords, label) {
-  var index;
   songs.push([label, chords]);
   labels.push(label);
-  for (index = 0; index < chords.length; index++) {
-    if (!allChords.includes(chords[index])) {
-      allChords.push(chords[index]);
+
+  chords.forEach((chord) => {
+    if(!allChords.includes(chord)) {
+      allChords.push(chord);
     }
-  }
+  });
 
   if (Object.keys(labelCounts).includes(label)) {
     labelCounts[label] = labelCounts[label] + 1;
